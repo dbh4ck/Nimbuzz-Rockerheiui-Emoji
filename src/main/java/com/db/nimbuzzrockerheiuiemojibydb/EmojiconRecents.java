290c1fb7dab5b39b8a1e0cc9daaa3716219bbec8
Your Name <you@example.com>
2017-12-03 16:28:52 +0530

    Added By DB~@NC

---
+++ src/main/java/com/db/nimbuzzrockerheiuiemojibydb/EmojiconRecents.java
@@ -0,0 +1,13 @@
+package com.db.nimbuzzrockerheiuiemojibydb;
+
+import android.content.Context;
+
+import com.db.nimbuzzrockerheiuiemojibydb.emoji.Emojicon;
+
+/**
+ * Created by DB on 03-12-2017.
+ */
+
+public interface EmojiconRecents {
+    void addRecentEmoji(Context context, Emojicon emojicon);
+}
---
+++ src/main/java/com/db/nimbuzzrockerheiuiemojibydb/EmojiconRecentsGridFragment.java
@@ -0,0 +1,70 @@
+package com.db.nimbuzzrockerheiuiemojibydb;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.GridView;
+
+import com.db.nimbuzzrockerheiuiemojibydb.emoji.Emojicon;
+
+/**
+ * Created by DB on 03-12-2017.
+ */
+
+public class EmojiconRecentsGridFragment extends EmojiconGridFragment implements EmojiconRecents {
+    private EmojiconAdapter mAdapter;
+    private boolean mUseSystemDefault = false;
+
+    private static final String USE_SYSTEM_DEFAULT_KEY = "useSystemDefaults";
+
+    protected static EmojiconRecentsGridFragment newInstance() {
+        return newInstance(false);
+    }
+
+    protected static EmojiconRecentsGridFragment newInstance(boolean useSystemDefault) {
+        EmojiconRecentsGridFragment fragment = new EmojiconRecentsGridFragment();
+        Bundle bundle = new Bundle();
+        bundle.putBoolean(USE_SYSTEM_DEFAULT_KEY, useSystemDefault);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mUseSystemDefault = getArguments().getBoolean(USE_SYSTEM_DEFAULT_KEY);
+        } else {
+            mUseSystemDefault = false;
+        }
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        EmojiconRecentsManager recents = EmojiconRecentsManager
+                .getInstance(view.getContext());
+
+        mAdapter = new EmojiconAdapter(view.getContext(), recents, mUseSystemDefault);
+        GridView gridView = (GridView) view.findViewById(R.id.Emoji_GridView);
+        gridView.setAdapter(mAdapter);
+        gridView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mAdapter = null;
+    }
+
+    @Override
+    public void addRecentEmoji(Context context, Emojicon emojicon) {
+        EmojiconRecentsManager recents = EmojiconRecentsManager
+                .getInstance(context);
+        recents.push(emojicon);
+
+        // notify dataset changed
+        if (mAdapter != null)
+            mAdapter.notifyDataSetChanged();
+    }
+
+}
